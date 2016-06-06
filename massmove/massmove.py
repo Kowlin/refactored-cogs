ea1c413183edb35c2b15f6b99026904c9c448852
Kowlin <boxedpp@gmail.com>
2016-06-06 10:39:29 +0200

    Massmove Alpha

---
+++ massmove/massmove.py
@@ -0,0 +1,43 @@
+import discord
+from discord.ext import commands
+from .utils import checks
+import asyncio
+import logging
+
+log = logging.getLogger('red.massmove')
+
+class Massmove:
+    """Massmove users to another voice channel"""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(pass_context=True)
+    @checks.admin_or_permissions(move_members=True)
+    async def massmove(self, ctx, from_channel: discord.Channel, to_channel: discord.Channel):
+        """Massmove users to another voice channel"""
+        # check if channels are voice channels. Or moving will be very... interesting...
+        type_from = str(from_channel.type)
+        type_to = str(to_channel.type)
+        if type_from == 'text':
+            await self.bot.say('{} is not a valid voice channel'.format(from_channel.name))
+            log.debug('SID: {}, from_channel not a voice channel'.format(from_channel.server.id))
+        elif type_to == 'text':
+            await self.bot.say('{} is not a valid voice channel'.format(to_channel.name))
+            log.debug('SID: {}, to_channel not a voice channel'.format(to_channel.server.id))
+        else:
+            try:
+                log.debug('Starting move on SID: {}'.format(from_channel.server.id))
+                for member in from_channel.voice_members:
+                    await self.bot.move_member(member, to_channel)
+                    log.debug('Member {} moved to channel {}'.format(member.id, to_channel.id))
+                    asyncio.sleep(0.5)
+            except discord.Forbidden:
+                await self.bot.say('I have no permission to move members.')
+            except discord.HTTPException:
+                await self.bot.say('A error occured. Please try again')
+
+
+def setup(bot):
+    n = Massmove(bot)
+    bot.add_cog(n)
