ecc2b2bbd24c53009cdb7057e3b9b29beb2855e0
b4xdored <tataviruzhahaha@gmail.com>
2017-03-11 10:09:35 -0500

    Update and rename antilink/antilink.py to antiinvites.py

--- antiinvites.py
+++ antiinvites.py
@@ -8,42 +8,42 @@ import re
 import asyncio
 
 
-class Antilink:
-    """Blocks Discord invite links from users who don't have the permission 'Manage Messages'"""
+class Antiinvite:
+    """Blocks discord invites from normal users.'"""
 
     def __init__(self, bot):
         self.bot = bot
-        self.location = 'data/antilink/settings.json'
+        self.location = 'data/antiinvites/settings.json'
         self.json = dataIO.load_json(self.location)
         self.regex = re.compile(r"<?(https?:\/\/)?(www\.)?(discord\.gg|discordapp\.com\/invite)\b([-a-zA-Z0-9/]*)>?")
         self.regex_discordme = re.compile(r"<?(https?:\/\/)?(www\.)?(discord\.me\/)\b([-a-zA-Z0-9/]*)>?")
 
     @commands.group(pass_context=True, no_pm=True)
-    async def antilinkset(self, ctx):
-        """Manages the settings for antilink."""
+    async def antiinvite(self, ctx):
+        """Antilink settings"""
         serverid = ctx.message.server.id
         if ctx.invoked_subcommand is None:
             await send_cmd_help(ctx)
         if serverid not in self.json:
             self.json[serverid] = {'toggle': False, 'message': '', 'dm': False}
 
-    @antilinkset.command(pass_context=True, no_pm=True)
+    @antiinvite.command(pass_context=True, no_pm=True)
     @checks.admin_or_permissions(administrator=True)
     async def toggle(self, ctx):
-        """Enable/disables antilink in the server"""
+        """Enable/disables antilink in your server"""
         serverid = ctx.message.server.id
         if self.json[serverid]['toggle'] is True:
             self.json[serverid]['toggle'] = False
-            await self.bot.say('Antilink is now disabled')
+            await self.bot.say('Antilink is now completly disabled on this server.')
         elif self.json[serverid]['toggle'] is False:
             self.json[serverid]['toggle'] = True
-            await self.bot.say('Antilink is now enabled')
+            await self.bot.say('Antilink is now fully enabled,👀 make sure to give "manage message" permission to the bot.')
         dataIO.save_json(self.location, self.json)
 
-    @antilinkset.command(pass_context=True, no_pm=True)
+    @antiinvite.command(pass_context=True, no_pm=True)
     @checks.admin_or_permissions(administrator=True)
     async def message(self, ctx, *, text):
-        """Set the message for when the user sends a illegal discord link"""
+        """Set the message when the user send invite links"""
         serverid = ctx.message.server.id
         self.json[serverid]['message'] = text
         dataIO.save_json(self.location, self.json)
@@ -51,7 +51,7 @@ class Antilink:
         if self.json[serverid]['dm'] is False:
             await self.bot.say('Remember: Direct Messages on removal is disabled!\nEnable it with ``antilinkset toggledm``')
 
-    @antilinkset.command(pass_context=True, no_pm=True)
+    @antiinvite.command(pass_context=True, no_pm=True)
     @checks.admin_or_permissions(administrator=True)
     async def toggledm(self, ctx):
         serverid = ctx.message.server.id
@@ -90,12 +90,12 @@ class Antilink:
 
 
 def check_folder():
-    if not os.path.exists('data/antilink'):
-        os.makedirs('data/antilink')
+    if not os.path.exists('data/antiinvites'):
+        os.makedirs('data/antiinvites')
 
 
 def check_file():
-    f = 'data/antilink/settings.json'
+    f = 'data/antiinvites/settings.json'
     if dataIO.is_valid_json(f) is False:
         dataIO.save_json(f, {})
 
@@ -103,6 +103,6 @@ def check_file():
 def setup(bot):
     check_folder()
     check_file()
-    n = Antilink(bot)
+    n = antiinvites(bot)
     bot.add_cog(n)
     bot.add_listener(n._new_message, 'on_message')
