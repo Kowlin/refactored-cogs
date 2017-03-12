8f8983438aeda074daed3a6d63e10b0ba52a9f46
Kowlin <boxedpp@gmail.com>
2017-03-13 00:21:56 +0100

    World's dumbest bug fix...

--- punish/punish.py
+++ punish/punish.py
@@ -20,7 +20,7 @@ log = logging.getLogger('red.punish')
 class Punish:
     """Adds the ability to punish users."""
 
-    __author__ "Kowlin"
+    __author__ = "Kowlin"
     __version__ = "P-v1.0-LTS"
 
     # --- Format
